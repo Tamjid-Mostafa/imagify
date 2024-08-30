e4259f046af2a28f84dd9c152c3773edb2694eba
Tamjid Mostafa <tamjid430@gmail.com>
2024-08-30 13:06:26 +0600

    git added

---
+++ src/lib/database/models/image.model.ts
@@ -0,0 +1,41 @@
+import { Document, Schema, model, models } from "mongoose";
+
+import { Types } from "mongoose";
+
+export interface IImage extends Document {
+  title: string;
+  tranformationType: string;
+  publicId: string;
+  secureUrl: string;
+  width?: number;
+  height?: number;
+  config?: object;
+  transformationUrl?: string;
+  aspectRatio?: string;
+  color?: string;
+  prompt?: string;
+  author?: Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const ImageSchema = new Schema({
+  title: { type: String, required: true },
+  tranformationType: { type: String, required: true },
+  publicId: { type: String, required: true },
+  secureUrl: { type: URL, required: true },
+  width: { type: Number },
+  height: { type: Number },
+  config: { type: Object },
+  transformationUrl: { type: URL },
+  aspectRatio: { type: String },
+  color: { type: String },
+  prompt: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const Image = models?.Image || model("Image", ImageSchema);
+
+export default Image;
